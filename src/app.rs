91f5e61f52f52bd05c4ef883d7f5202e650f736a
h5p9sl <21267024+h5p9sl@users.noreply.github.com>
2020-10-03 15:42:17 -0600

    Move clap code to app.rs
    Clean up main.rs

---
+++ src/app.rs
@@ -0,0 +1,42 @@
+use clap::{crate_version, AppSettings, Arg, ArgMatches};
+
+pub fn create_app() -> ArgMatches<'static> {
+    clap::App::new("Morse Bot")
+        .setting(AppSettings::ColoredHelp)
+        .version(crate_version!())
+        .args(&[
+            Arg::with_name("MESSAGE")
+                .required_unless_one(&["file", "list_keys"])
+                ,
+            Arg::with_name("wpm")
+                .default_value("15")
+                .help("Words per minute - how fast the virtual keyer goes")
+                .long_help("Words per minute - how long a single time unit is (one \"dot\"/\"dit\"), which defines how fast the virtual keyer \"types\" out the morse code message.")
+                .short("w")
+                .long("wpm")
+                .takes_value(true)
+                ,
+            Arg::with_name("delay")
+                .help("Delay before starting")
+                .long_help("Specifies how long the program should wait before starting the virtual keyer (the playback of the morse code)")
+                .short("d")
+                .long("delay")
+                .takes_value(true)
+                ,
+            Arg::with_name("mouse")
+                .short("m")
+                .long("mouse")
+                .help("Defines which mouse button to use for sending morse, possible values are non-negative numbers")
+                .takes_value(true)
+                .default_value("1")
+                .required_unless("key")
+                ,
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .help("Load message from file")
+                .takes_value(true)
+        ])
+        .after_help("To view a summary of a flag/option, use `-h`. To view a detailed description of a flag/option, use `--help`")
+        .get_matches()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,41 @@
 #![feature(test)]
 
-use clap::{crate_version, Arg};
 use inputbot::MouseButton;
-use std::time::Duration;
+use std::{str::FromStr, time::Duration};
 
+mod app;
 mod morse;
 
 fn main() {
-    let matches = clap::App::new("Morse Bot")
-        .version(crate_version!())
-        .args(&[
-            Arg::with_name("MESSAGE")
-                .required_unless("file")
-                ,
-            Arg::with_name("wpm")
-                .default_value("15")
-                .help("Words per minute - how fast the virtual keyer goes")
-                .long_help("Words per minute - how long a single time unit is (one 'dit'/'.'), which defines how fast the virtual keyer \"types\" out the morse code message.")
-                .short("w")
-                .long("wpm")
-                .takes_value(true)
-                ,
-            Arg::with_name("delay")
-                .help("Delay before starting")
-                .long_help("Specifies how long the program should wait before starting the virtual keyer (the playback of the morse code)")
-                .short("d")
-                .long("delay")
-                .takes_value(true)
-                ,
-            Arg::with_name("file")
-                .short("f")
-                .long("file")
-                .help("Load message from file")
-                .takes_value(true)
-        ])
-        .after_help("To view a summary of a flag/option, use `-h`. To view a detailed description of a flag/option, use `--help`")
-        .get_matches();
-
-    let mut msg = String::from(matches.value_of("MESSAGE").unwrap_or(""));
+    let matches = app::create_app();
     let alphabet = morse::Alphabet::new();
 
-    use std::str::FromStr;
+    let msg = {
+        if let Some(file) = matches.value_of("file") {
+            std::fs::read_to_string(file).unwrap()
+        } else {
+            matches.value_of("MESSAGE").unwrap().to_string()
+        }
+    };
+
+    // Wait for `delay` seconds if specified in app args
     if let Some(delay) = matches.value_of("delay") {
         std::thread::sleep(Duration::from_secs_f32(f32::from_str(delay).unwrap()));
     }
-    if let Some(file) = matches.value_of("file") {
-        msg = std::fs::read_to_string(file).unwrap();
-    }
+
     let wpm = u32::from_str(matches.value_of("wpm").unwrap()).unwrap();
+
+    let button =
+        MouseButton::OtherButton(u32::from_str(matches.value_of("button").unwrap()).unwrap());
+
     morse::Executor::new()
         .with_message(alphabet.to_morse(msg))
         .with_wpm(wpm)
-        .with_callback(|state| {
+        .with_callback(move |state| {
             if state == morse::State::Down {
-                MouseButton::LeftButton.press();
+                button.press();
             } else {
-                MouseButton::LeftButton.release();
+                button.release();
             }
         })
         .execute();
